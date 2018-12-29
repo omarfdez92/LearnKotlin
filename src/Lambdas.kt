2bae14a92fa55bdea33cbe99f27c340c678638ec
Omar Parra <omarfdez92@gmail.com>
2018-12-28 18:51:33 -0600

    Trying stuff with git and intelliJ

---
+++ src/Lambdas.kt
@@ -0,0 +1,13 @@
+/*
+fun main(args: Array<String>) {
+    //Simple lambda
+    val printMessage = { message: String -> println(message) }
+    //same but. . . we have benefits using lambda
+    fun printMessage( message: String ){
+        println(message)
+    }
+}*/
+
+fun main(args: Array<String>) {
+    val printMessage = {}
+}
